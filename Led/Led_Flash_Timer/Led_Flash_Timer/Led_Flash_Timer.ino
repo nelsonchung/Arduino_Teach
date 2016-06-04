444b88aae939244b91cbf52efa1c91bbf17540e3
nelsonchung <chihchun.chung@gmail.com>
2016-06-04 21:10:25 +0800

    使用其他function，可以做到重複幾次的效果

--- Led/Led_Flash_Timer/Led_Flash_Timer/Led_Flash_Timer.ino
+++ Led/Led_Flash_Timer/Led_Flash_Timer/Led_Flash_Timer.ino
@@ -6,7 +6,11 @@ void setup() {
   // put your setup code here, to run once:
 
     pinMode(13, OUTPUT);
-    t13.oscillate(13, 500, HIGH); //控制13, 初始化是HIGH
+    //重複閃滅
+    //t13.oscillate(13, 500, HIGH); //控制13, 初始化是HIGH
+    //重複閃滅 repeattime 次數
+    int repeattime = 10;
+    t13.oscillate(13, 500, HIGH, repeattime); //控制13, 初始化是HIGH
 }
 
 void loop() {
