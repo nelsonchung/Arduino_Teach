55104dcf15fcb506e2edb3c4e46fac62acf55115
nelson_chung <nelson_chung@askey.com.tw>
2017-01-23 09:09:54 +0800

    CheckSlaveID.ino 檢查slave ID I2C_Master Master,角色,可以對slave下command

---
+++ I2C/Master/CheckSlaveID/CheckSlaveID/CheckSlaveID.ino
@@ -0,0 +1,62 @@
+/*
+ * Ref: http://arduino169.blogspot.tw/2015/07/arduino-i2c-1-i2c-address-i2c.html
+ */
+#include <Wire.h>
+ 
+#define SERIAL_BAUD 9600 
+ 
+void setup()
+{
+  Wire.begin();
+  
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("I2C Scanner started");
+  Serial.println();
+}
+  
+  
+void loop()
+{
+  uint8_t error, i2cAddress, devCount, unCount;
+  
+  Serial.println("Scanning...");
+  
+  devCount = 0;
+  unCount = 0;
+  //for(i2cAddress = 1; i2cAddress < 127; i2cAddress++ )
+  for(i2cAddress = 0x01; i2cAddress < 0x7F; i2cAddress++ )
+  {
+    Wire.beginTransmission(i2cAddress);
+    error = Wire.endTransmission();
+  
+    if (error == 0)
+    {
+      Serial.print("I2C device found at 0x");
+      if (i2cAddress<16) Serial.print("0");
+      Serial.println(i2cAddress,HEX);
+      devCount++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknow error at 0x");
+      if (i2cAddress<16) Serial.print("0");
+      Serial.println(i2cAddress,HEX);
+      unCount++;
+    }    
+  }
+ 
+  if (devCount + unCount == 0)
+    Serial.println("No I2C devices found\n");
+  else {
+    Serial.println();
+    Serial.print(devCount);
+    Serial.print(" device(s) found");
+    if (unCount > 0) {
+      Serial.print(", and unknown error in ");
+      Serial.print(unCount);
+      Serial.print(" address");
+    }
+    Serial.println();
+  }
+  delay(10000);
+}
---
+++ I2C/Master/I2C_Master/I2C_Master.ino
@@ -0,0 +1,28 @@
+//Ref: http://coopermaa2nd.blogspot.tw/2011/12/arduino-i2c.html
+// master_sender.ino
+// Refer to the "slave_receiver" example for use with this
+#include <Wire.h>
+
+const int SLAVE_ADDRESS = 0x8A;
+char incomingByte = 0;
+
+void setup() {  
+  Wire.begin();         // join I2C bus as a Master
+  
+  Serial.begin(9600);
+  Serial.println("Type something to send:");
+}
+
+void loop() {
+}
+
+void serialEvent()
+{
+  // read one byte from serial port
+  incomingByte = Serial.read();
+
+  // send the received data to slave
+  Wire.beginTransmission(SLAVE_ADDRESS);
+  Wire.write(incomingByte);
+  Wire.endTransmission();
+}
