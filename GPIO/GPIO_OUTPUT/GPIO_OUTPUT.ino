f3f4da6225b312238806f2359b63be154142ed02
nelson_chung <nelson_chung@askey.com.tw>
2017-02-10 10:56:23 +0800

    新增gpio範例,以led為例 增加示意圖,表達硬體拉線

---
+++ GPIO/GPIO_OUTPUT/GPIO_OUTPUT.ino
@@ -0,0 +1,15 @@
+const int BLUE_LED = 12;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(BLUE_LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(BLUE_LED, HIGH);
+  delay(2000);
+  digitalWrite(BLUE_LED, LOW);
+  delay(2000);
+}
